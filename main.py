bb1a68e8d057699538259f3440ac40df13e76113
Freddie Beasley <fwbeasley3@yahoo.com>
2019-04-21 13:43:12 -0500

    halfway

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__)                         #database setup
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:password@localhost:8889/get-it-done'   # string to connect to database
+app.config['SQLALCHEMY_ECHO'] = True    # echoes seql commands from squlalchemy
+db = SQLAlchemy(app)                 
+                                
+class Task(db.Model):
 
-tasks =[]
+    id = db.Column(db.Integer, primary_key=True)    
+    name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)  
+
+    def __init__(self,name):     
+        self.name=name 
+        self.completed = False  
+
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
-    return render_template('todos.html',title="Get It Done!", tasks=tasks)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
+        
+    tasks = Task.query.filter_by(completed = False).all()
+    completed_tasks = Task.query.filter_by(completed = True).all()
+    return render_template('todos.html',title="Get It Done!",
+     tasks=tasks, completed_tasks=completed_tasks)
 
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
 
-app.run()
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ =='__main__':
+    app.run()
